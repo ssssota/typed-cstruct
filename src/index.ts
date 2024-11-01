105d5abc93132ff18af00cb836f58d345a21f6d7
ssssota <tommysouta@gmail.com>
2024-11-01 21:19:36 +0900

    fix: set package exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import { Struct as StructInternal } from "./builders/struct.js";
 export const Struct = StructInternal<[]>; // hide the generic type
+export * from "./types.js";
+export * from "./builders/index.js";
+export * from "./utils.js";
